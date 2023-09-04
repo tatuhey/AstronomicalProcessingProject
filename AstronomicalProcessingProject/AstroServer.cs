0b5dc7bf34076d7f07d62179b8c90e028c644152
RAIHAN ABDILLAH <30065695@tafe.wa.edu.au>
2023-09-04 10:47:46 +0800

    added IAstroContract and AstroServer

---
+++ AstronomicalProcessingProject/AstroServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AstroMath;
+
+namespace AstronomicalProcessingProject
+{
+    public class AstroServer : IAstroContract
+    {
+        public double StarVelocity(double ObsWave, double RestWave)
+        {
+            double result = AstroMath.AstroMath.StarVelocity(ObsWave, RestWave);
+            return result;
+        }
+
+        public double StarDistance(double Parallax)
+        {
+            double result = AstroMath.AstroMath.StarDistance(Parallax);
+            return result;
+        }
+
+        public double TempInKelvin(double Cel)
+        {
+            double result = AstroMath.AstroMath.TempInKelvin(Cel);
+            return result;
+        }
+
+        public double EventHorizon(double mass)
+        {
+            double result = AstroMath.AstroMath.EventHorizon(mass);
+            return result;
+        }
+    }
+}
---
+++ AstronomicalProcessingProject/IAstroContract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace AstronomicalProcessingProject
+{
+    [ServiceContract]
+    internal interface IAstroContract
+    {
+        [OperationContract]
+        double StarVelocity(double ObsWave, double RestWave);
+
+        [OperationContract]
+        double StarDistance(double Parallax);
+
+        [OperationContract]
+        double TempInKelvin(double Celcius);
+
+        [OperationContract]
+        double EventHorizon(double BlackholeMass);
+
+    }
+}
