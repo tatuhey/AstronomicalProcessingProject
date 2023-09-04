95b0be629c2cb9f65296d2a105f499224e202a69
RAIHAN ABDILLAH <30065695@tafe.wa.edu.au>
2023-09-04 10:17:13 +0800

    Barebone visual

--- AstronomicalProcessingProject/Form1.Designer.cs
+++ AstronomicalProcessingProject/Form1.Designer.cs
@@ -28,13 +28,398 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEnglish = new System.Windows.Forms.Button();
+            this.btnFrench = new System.Windows.Forms.Button();
+            this.btnGerman = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.btnVelocity = new System.Windows.Forms.Button();
+            this.btnDistance = new System.Windows.Forms.Button();
+            this.btnKelvin = new System.Windows.Forms.Button();
+            this.btnRadius = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.rbLight = new System.Windows.Forms.RadioButton();
+            this.rbDark = new System.Windows.Forms.RadioButton();
+            this.gbVisualStyle = new System.Windows.Forms.GroupBox();
+            this.gbVisualStyle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEnglish
+            // 
+            this.btnEnglish.Location = new System.Drawing.Point(587, 350);
+            this.btnEnglish.Name = "btnEnglish";
+            this.btnEnglish.Size = new System.Drawing.Size(75, 51);
+            this.btnEnglish.TabIndex = 0;
+            this.btnEnglish.Text = "English";
+            this.btnEnglish.UseVisualStyleBackColor = true;
+            // 
+            // btnFrench
+            // 
+            this.btnFrench.Location = new System.Drawing.Point(668, 350);
+            this.btnFrench.Name = "btnFrench";
+            this.btnFrench.Size = new System.Drawing.Size(75, 51);
+            this.btnFrench.TabIndex = 1;
+            this.btnFrench.Text = "French";
+            this.btnFrench.UseVisualStyleBackColor = true;
+            // 
+            // btnGerman
+            // 
+            this.btnGerman.Location = new System.Drawing.Point(749, 350);
+            this.btnGerman.Name = "btnGerman";
+            this.btnGerman.Size = new System.Drawing.Size(75, 51);
+            this.btnGerman.TabIndex = 2;
+            this.btnGerman.Text = "German";
+            this.btnGerman.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(321, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(321, 237);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(37, 366);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(37, 500);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(321, 366);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 9;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(321, 500);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 10;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(34, 237);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 11;
+            // 
+            // btnVelocity
+            // 
+            this.btnVelocity.Location = new System.Drawing.Point(34, 180);
+            this.btnVelocity.Name = "btnVelocity";
+            this.btnVelocity.Size = new System.Drawing.Size(75, 23);
+            this.btnVelocity.TabIndex = 13;
+            this.btnVelocity.Text = "Calculate";
+            this.btnVelocity.UseVisualStyleBackColor = true;
+            // 
+            // btnDistance
+            // 
+            this.btnDistance.Location = new System.Drawing.Point(321, 180);
+            this.btnDistance.Name = "btnDistance";
+            this.btnDistance.Size = new System.Drawing.Size(75, 23);
+            this.btnDistance.TabIndex = 14;
+            this.btnDistance.Text = "Calculate";
+            this.btnDistance.UseVisualStyleBackColor = true;
+            // 
+            // btnKelvin
+            // 
+            this.btnKelvin.Location = new System.Drawing.Point(37, 443);
+            this.btnKelvin.Name = "btnKelvin";
+            this.btnKelvin.Size = new System.Drawing.Size(75, 23);
+            this.btnKelvin.TabIndex = 15;
+            this.btnKelvin.Text = "Calculate";
+            this.btnKelvin.UseVisualStyleBackColor = true;
+            // 
+            // btnRadius
+            // 
+            this.btnRadius.Location = new System.Drawing.Point(321, 436);
+            this.btnRadius.Name = "btnRadius";
+            this.btnRadius.Size = new System.Drawing.Size(75, 23);
+            this.btnRadius.TabIndex = 16;
+            this.btnRadius.Text = "Calculate";
+            this.btnRadius.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Observed Wavelength";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Rest Wavelength (λo)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Parallax Angle (P)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Celsius (°C)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(318, 350);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Mass of the Blackhole (M)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(318, 482);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Schwarzschild Radius (R)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Star Velocity (V)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(318, 221);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Distance (D)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(34, 484);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Kelvin (K)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(29, 43);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(148, 26);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Star Velocity";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(316, 43);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(156, 26);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Star Distance";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(32, 306);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(245, 26);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Temperature in Kelvin";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(316, 306);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(162, 26);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Event Horizon";
+            // 
+            // rbLight
+            // 
+            this.rbLight.AutoSize = true;
+            this.rbLight.Location = new System.Drawing.Point(6, 19);
+            this.rbLight.Name = "rbLight";
+            this.rbLight.Size = new System.Drawing.Size(78, 17);
+            this.rbLight.TabIndex = 30;
+            this.rbLight.TabStop = true;
+            this.rbLight.Text = "Light Mode";
+            this.rbLight.UseVisualStyleBackColor = true;
+            // 
+            // rbDark
+            // 
+            this.rbDark.AutoSize = true;
+            this.rbDark.Location = new System.Drawing.Point(6, 42);
+            this.rbDark.Name = "rbDark";
+            this.rbDark.Size = new System.Drawing.Size(78, 17);
+            this.rbDark.TabIndex = 31;
+            this.rbDark.TabStop = true;
+            this.rbDark.Text = "Dark Mode";
+            this.rbDark.UseVisualStyleBackColor = true;
+            // 
+            // gbVisualStyle
+            // 
+            this.gbVisualStyle.Controls.Add(this.rbLight);
+            this.gbVisualStyle.Controls.Add(this.rbDark);
+            this.gbVisualStyle.Location = new System.Drawing.Point(587, 237);
+            this.gbVisualStyle.Name = "gbVisualStyle";
+            this.gbVisualStyle.Size = new System.Drawing.Size(108, 69);
+            this.gbVisualStyle.TabIndex = 32;
+            this.gbVisualStyle.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(920, 643);
+            this.Controls.Add(this.gbVisualStyle);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRadius);
+            this.Controls.Add(this.btnKelvin);
+            this.Controls.Add(this.btnDistance);
+            this.Controls.Add(this.btnVelocity);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGerman);
+            this.Controls.Add(this.btnFrench);
+            this.Controls.Add(this.btnEnglish);
+            this.Name = "Form1";
+            this.Text = "Astronomnical Processing";
+            this.gbVisualStyle.ResumeLayout(false);
+            this.gbVisualStyle.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEnglish;
+        private System.Windows.Forms.Button btnFrench;
+        private System.Windows.Forms.Button btnGerman;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button btnVelocity;
+        private System.Windows.Forms.Button btnDistance;
+        private System.Windows.Forms.Button btnKelvin;
+        private System.Windows.Forms.Button btnRadius;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.RadioButton rbLight;
+        private System.Windows.Forms.RadioButton rbDark;
+        private System.Windows.Forms.GroupBox gbVisualStyle;
     }
 }
 
